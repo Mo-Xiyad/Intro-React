aa2a148aca3689e9d8b51918f142b79fe4c564c2
Mo-Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-09-27 14:28:08 +0200

    notes

---
+++ first-app/src/Hello.jsx
@@ -0,0 +1,11 @@
+const hello = ()=> {
+    return (
+        <div>
+            <h1>helloo this is my first react component</h1>
+        </div>
+    )
+}
+
+export const name = 'Zee'
+
+export default hello
